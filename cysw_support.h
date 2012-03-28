1c560b0b2f080652d2f67fcb3d94fa6acd2c6108
Robert Kern <rkern@enthought.com>
2012-03-28 15:24:56 +0100

    ENH: Add an ExplainedCassowaryError for handling errors that hold the problematic constraints.

--- cysw_support.h
+++ cysw_support.h
@@ -1,16 +1,16 @@
+#include "stdint.h"
 #include <string>
-#include <sstream>
+#include <vector>
 #include "cassowary/SimplexSolver.h"
-#include "cassowary/Errors.h"
 #include "cassowary/LinearExpression.h"
 #include "cassowary/Constraint.h"
 
 
-
+std::vector<size_t> get_cpp_exception_constraint_pointers();
 std::string get_cpp_exception_message();
 std::string solver_str(SimplexSolver *solver);
 P_Constraint *newLinearEquation(const P_LinearExpression &lhs, const P_LinearExpression &rhs, const Strength &strength, double weight);
 P_Constraint *newLinearInequality(const P_LinearExpression &lhs, CnRelation op, const P_LinearExpression &rhs, const Strength &strength, double weight);
 P_LinearExpression newLinearExpression(double constant);
 void delete_P_Constraint(P_Constraint *pcn);
-
+size_t get_P_Constraint_addr(P_Constraint *pcn);
